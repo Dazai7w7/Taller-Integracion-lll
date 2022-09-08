b878350099403c08deea85689acc374a36726196
Singlaaar <nicolascalquin25@gmail.com>
2022-09-08 13:12:12 -0300

    Create card.js
    Intento de tarjeta de usuario

---
+++ Proyecto/src/components/card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Card extends React.Component {
+    render(){
+        return(
+            <div>
+                <div>
+                    <h1>Username</h1>
+                    <p>game list</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
